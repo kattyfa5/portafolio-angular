0aeab5a7a971c92fc63d47907e57f430058fea8e
kattyfa <kattyfa55@gmail.com>
2020-04-23 12:56:49 -0500

    firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,27 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient){ 
-    // console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     // leer el archivo Json
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
+    });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-6d84a.firebaseio.com/equipo.json')
+    .subscribe((resp: any[]) => {
+      this.equipo = resp;
       console.log(resp);
     });
-
   }
 }
